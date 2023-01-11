41df2dc35735e3a97d479756789b0eea1bdb8321
ishoborabyose <ishoborabyosebeatrice@gmail.com>
2023-01-11 15:47:27 +0200

    add new files

--- components/wrappers/Wrapper.jsx
+++ components/wrappers/Wrapper.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 function wrapper({ children, styles }) {
   return (
     <section className='bg-[#DDDDDD]'>
-          <div className={`max-w-6xl px-3 m-auto ${styles}`}>
+          <div className={`max-w-6xl px-3 mx-auto ${styles}`}>
           {children}
           </div>
     </section>
